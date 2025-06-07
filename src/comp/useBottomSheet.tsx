b9be3a39be5e87668a96d0cb3ef5377d100264bf
TRINITY-21 <agyemanjoseph21@gmail.com>
2025-06-07 22:08:40 +0300

    bottomsheet updated

--- src/comp/useBottomSheet.tsx
+++
@@ -1,5 +0,0 @@
-import { useContext } from 'react';
-import BottomSheetContext, { BottomSheetType } from './context';
-
-const useBottomSheet = (): BottomSheetType => useContext(BottomSheetContext);
-export default useBottomSheet;
